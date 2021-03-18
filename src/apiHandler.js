2d531a02d45529893c9561704ea1b4434e2c620b
PatrykMlodawski <pk.mlodawski@gmail.com>
2021-03-18 18:38:09 +0100

    Added initialization of currency options from API

--- src/apiHandler.js
+++ src/apiHandler.js
@@ -1,12 +1,23 @@
-const URL = 'https://api.exchangeratesapi.io/history';
+const URL = 'https://api.exchangeratesapi.io/';
 
-async function getData(startDate, endDate) {
+async function getData({ startDateStr, endDateStr }) {
   try {
     const response = await fetch(
-      `${URL}?start_at=${startDate}&end_at=${endDate}&base=USD`
+      `${URL}history?start_at=${startDateStr}&end_at=${endDateStr}&base=USD`
     );
     const data = await response.json();
     console.log(data);
+  } catch (error) {
+    console.log('Fetch error:', error);
+  }
+}
+
+export async function initializeOptions(setOptions) {
+  try {
+    const response = await fetch(`${URL}latest`);
+    const data = await response.json();
+    const options = Object.keys(data.rates).sort();
+    setOptions(options);
   } catch (error) {
     console.log('Fetch error:', error);
   }
--- src/index.js
+++ src/index.js
@@ -1,10 +1,28 @@
-import getData from './apiHandler';
+import getData, { initializeOptions } from './apiHandler';
 
-const date = new Date();
-const startDate = new Date(date);
-startDate.setDate(startDate.getDate() - 30);
-const dateStr = date.toISOString().substring(0, 10);
-const startDateStr = startDate.toISOString().substring(0, 10);
-console.log(dateStr, startDateStr);
+const baseSelect = document.getElementById('base-select');
+const currencySelect = document.getElementById('currency-select');
 
-getData(startDateStr, dateStr);
+function setOptions(options) {
+  options.forEach((element) => {
+    const option = document.createElement('option');
+    option.value = element;
+    option.innerText = element;
+    baseSelect.appendChild(option);
+    currencySelect.appendChild(option.cloneNode(true));
+  });
+}
+
+function setDate() {
+  const currentDate = new Date();
+  const startDate = new Date(currentDate);
+  startDate.setDate(startDate.getDate() - 30);
+  const startDateStr = startDate.toISOString().substring(0, 10);
+  const endDateStr = currentDate.toISOString().substring(0, 10);
+
+  return { startDateStr, endDateStr };
+}
+
+initializeOptions(setOptions);
+
+getData(setDate());
